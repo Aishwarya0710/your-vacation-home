904f5a52004891f2fb990205fa9496e9dcdac4f7
Aishwarya0710 <aishwaryamagre@gmail.com>
2026-03-04 15:12:55 +0530

    Add the backend project

---
+++ vacation-home-backend/src/main/java/com/vacation_home_backend/vacation_home_backend/VacationHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vacation_home_backend.vacation_home_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationHomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationHomeBackendApplication.class, args);
+	}
+
+}
